4abda9aeeec8bf1ce8913f12ba58c5e5a5844296
MSD <123319753+msamet07@users.noreply.github.com>
2024-01-13 12:34:41 +0300

    reactivity kavramına giriş

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,8 +10,14 @@ export default {
     name: "App",
     components : {
         NavBar
+    },
+    data() {
+        return{
+            message:"Merhaba",
+            count:0,
+            isVisible:true
+        }
     }
-    
 }
 
 </script>
@@ -20,8 +26,9 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;600;700&family=Open+Sans&family=Rubik+Iso&family=Satisfy&display=swap');
 html,body {
 font-family: 'Mulish', sans-serif;
+/*font-family: 'Mulish', sans-serif;
 font-family: 'Open Sans', sans-serif;
 font-family: 'Rubik Iso', system-ui;
-font-family: 'Satisfy', cursive;
+font-family: 'Satisfy', cursive;*/
 }
 </style>
